6eaff912b864c49e2938e93fe568de12361db16e
mamagoudou <mamagoudou@gmail.com>
2020-11-24 23:48:43 +0100

    Init with Hello World and Project

---
+++ Project/src/MultiServerThread.java
@@ -0,0 +1,36 @@
+import java.net.*;
+import java.io.*;
+
+public class MultiServerThread extends Thread {
+    private Socket socket = null;
+
+    public MultiServerThread(Socket socket) {
+        super("MultiServerThread");
+        this.socket = socket;
+    }
+
+    public void run() {
+
+        try (
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream()));
+        ) {
+            String inputLine, outputLine;
+            ServerProtocol sp = new ServerProtocol();
+            outputLine = sp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = sp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye"))
+                    break;
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Project/src/ServerProtocol.java
@@ -0,0 +1,10 @@
+import java.net.*;
+import java.io.*;
+
+public class ServerProtocol {
+
+    public String processInput(String theInput) {
+        return "dumb";
+    }
+
+}
